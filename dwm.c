6d7794abf522562010a2625c2df8fb7225b9a8f7
Roy Långsjö <roy.langsjo@aalto.fi>
2024-12-18 15:28:26 +0200

    autostart stuff

--- dwm.c
+++ dwm.c
@@ -1395,8 +1395,8 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.dwm; ./autostart.sh &");
+	system("cd /home/langsjo/.dwm; ./autostart_blocking.sh");
+	system("cd /home/langsjo/.dwm; ./autostart.sh &");
 }
 
 void
